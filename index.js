dffc67efa749ff4f412b2eb659c7a6b37edaf009
Pedro Resch <34459903+rschpdr@users.noreply.github.com>
2021-08-10 19:07:32 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// Seu código aqui!
+
+const products = [
+  {
+    id: 1,
+    name: 'Iphone 11',
+    tags: ['smartphone', 'tech', 'apple']
+  },
+    {
+    id: 2,
+    name: 'Macbook Pro',
+    tags: ['laptop', 'tech', 'apple']
+  },
+    {
+    id: 3,
+    name: 'JBL Headphones',
+    tags: ['headphone', 'tech', 'jbl']
+  },
+    {
+    id: 4,
+    name: 'Nike Shorts',
+    tags: ['shorts', 'clothing', 'nike']
+  },
+];
+
+// Declare sua funçao aqui. Nao se esqueça de invocar a função para conseguir testar!
